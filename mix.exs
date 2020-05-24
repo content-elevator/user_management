bf4753928d6474ea300e4f1c4cef7db23001e6e7
daniftinca <daniftincaalexandru@gmail.com>
2020-05-24 13:50:43 +0300

    ci configs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule UserManagement.MixProject do
     [
       app: :user_management,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
