c4ccf5fc339caf80af323e86c777647014a1196d
tiboth <37446840+tiboth@users.noreply.github.com>
2020-05-31 01:01:55 +0300

    added cors plugin

--- lib/user_management_web/endpoint.ex
+++ lib/user_management_web/endpoint.ex
@@ -41,5 +41,6 @@ defmodule UserManagementWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug CORSPlug, origin: ["*"]
   plug UserManagementWeb.Router
 end
